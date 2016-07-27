2f93d6d4f086c5e8ff0036a26273707f0d58bf9b
smandler <sv.mandler@gmail.com>
2016-07-27 12:49:57 +1000

    Game
    A ball, a wall, and a picture

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonControl : MonoBehaviour {
+	private Renderer rd;
+	private GameObject door;
+	private int count;
+	public TextMesh text;
+
+	void Start()
+	{
+		rd = GetComponent<Renderer> ();
+		door = GameObject.Find ("Door");
+	}
+
+	void Update(){
+		InputText();
+	}
+
+	void InputText(){
+		foreach (char c in Input.inputString){
+			Debug.Log (c);
+			if (c == "\b" [0]) {
+				if (text.text.Length != 0)
+					text.text = text.text.Substring (0, text.text.Length - 1);
+			}
+			else 
+				text.text += c;
+		}
+	}
+
+
+	void OnTriggerEnter (Collider other) {
+		if (count == 0) {
+			//Debug.Log ("Hello");
+			door.SetActive (false);
+			transform.localPosition += new Vector3 (0, 0, 0.3f);
+			rd.material.color = new Color (1, 0, 0);
+		}
+		count++;
+
+	}
+
+	void OnTriggerExit (Collider other) {
+		count--;
+		if (count == 0) {
+		//	Debug.Log ("Bye");
+			door.SetActive (true);
+			transform.localPosition -= new Vector3 (0, 0, 0.3f);
+			rd.material.color = new Color (0, 1, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public int speed;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var direction = new Vector3 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0); 
+		rb.AddForce (direction * speed);
+	}
+}
